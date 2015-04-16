c3e02a7047f389866197e84fc68a78af4a4be003
Nathan Anderson <nallenanderson@gmail.com>
2015-04-16 12:35:02 -0300

    Refactored GMaps from App Layout

---
+++ app/assets/javascripts/googlemaps.js
@@ -0,0 +1,15 @@
+$(function initialize() {
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: new google.maps.LatLng(-33.4126843, -70.5886704),
+    zoom: 15,
+    disableDefaultUI: true,
+    scrollwheel: true,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: true,
+    mapTypeId: google.maps.MapTypeId.TERRAIN,
+    styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]}
+  var map = new google.maps.Map(mapCanvas, mapOptions)
+});
+google.maps.event.addDomListener(window, 'load', initialize);
