86f5e94b6f55b8ec8d1178b5e8c5c9eecac3494f
Nathan Anderson <nallenanderson@gmail.com>
2015-04-17 13:52:35 -0300

    Added market to google map

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -10,6 +10,14 @@ $(function initialize() {
     scaleControl: true,
     mapTypeId: google.maps.MapTypeId.TERRAIN,
     styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]}
-  var map = new google.maps.Map(mapCanvas, mapOptions)
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var image = '../assets/marker1.png';
+  var myLatLng = new google.maps.LatLng(-33.4126843, -70.5886704);
+  var myMarker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      icon: image
+  });
 });
+
 google.maps.event.addDomListener(window, 'load', initialize);
