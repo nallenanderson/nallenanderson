d9010b9be7a137a04736073303736b7e82086130
Nathan Anderson <nallenanderson@gmail.com>
2015-07-06 21:04:01 -0400

    Clean up unused views, controller methods, and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,9 @@ class PagesController < ApplicationController
 	def home
 	end
 
-	def explainrails
-	end
-
-	def projects
-	end
-
-	def about
-	end
-
 	def contact
 		@lead = Lead.new
-		#respond_to do |format|
-      #format.html { redirect_to root_path, notice: '¡Gracias por el mensaje!' }
-      #format.json { head :no_content }
-    #end
+		
 	end
 	
 end
